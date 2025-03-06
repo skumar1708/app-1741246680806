68cc71579cb8641797d09077ce9c74e058f9b1b2
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-06 13:09:01 +0530

    Automated commit: backend/routes/snackRoutes.js

---
+++ backend/routes/snackRoutes.js
@@ -0,0 +1,1 @@
+const express = require('express'); const router = express.Router(); const snackController = require('../controllers/snackController'); router.get('/', snackController.getSnacks); router.post('/', snackController.createSnack); router.put('/:id', snackController.updateSnack); router.delete('/:id', snackController.deleteSnack); module.exports = router;
