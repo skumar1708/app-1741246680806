84083be95377e132da594e60b689681698e12e57
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-06 13:09:09 +0530

    Automated commit: frontend/src/components/GameBoard.js

---
+++ frontend/src/components/GameBoard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function GameBoard() {
+  return (
+    <div className="game-board">
+      <h2>Game Board</h2>
+      {/* Add game board elements here */}
+    </div>
+  );
+}
+
+export default GameBoard;
